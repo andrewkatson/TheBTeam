72ef578785a1dcdfffbd6404ed5e7227f89d4b98
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-10-18 12:01:16 -0400

    add documentation in GameLogic

--- include/GameLogic.hpp
+++ include/GameLogic.hpp
@@ -1,3 +1,11 @@
+/*
+  GameLogic.hpp
+  
+  Purpose: Handle all logical processes for Food Fight.
+
+  @author Andrew Katson, Jeremy Elkayam
+ */
+
 #ifndef GAMELOGIC_H
 #define GAMELOGIC_H
 
@@ -6,12 +14,26 @@
 
 class GameLogic{
 private:
+  //Store the game's instance of the BoardManager class that the game logic
+  //needs for generation and playing of game boards. 
   unique_ptr<BoardManager> boardManager;
+  //Store the instance of the GameState class that the logic needs to remember
+  //and modify the game's current state.
   unique_ptr<GameState> state;
 
 public:
+  /*
+    Constructor. Initialize game, setting up instance variables.
+   */
   GameLogic();
-  
+
+  /*
+    Called once every game loop. Updates the game's happenings according to the
+    time since the last loop.
+
+    @param deltaS The amount of time in seconds since the last loop began
+                  execution.
+   */
   void updateGameLogic(float deltaS);
 };
 
--- src/GameLogic.cpp
+++ src/GameLogic.cpp
@@ -1,10 +1,19 @@
+/*
+  GameLogic.cpp
+  
+  Purpose: Handle all logical processes for Food Fight.
+
+  @author Andrew Katson, Jeremy Elkayam
+ */
 #include "GameLogic.hpp"
 
+//Constructor.
 GameLogic::GameLogic(){
 
   this -> boardManager = unique_ptr<BoardManager>(new BoardManager());
 }
 
+//Called once every loop. Update according to elapsed time.
 void GameLogic::updateGameLogic(float deltaS){
   
   
