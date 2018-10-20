fac27d29c6bef5d12b232f1faae9d108da3e3c32
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-10-20 17:19:04 -0400

    changed compview files

--- src/CompView.cpp
+++ src/CompView.cpp
@@ -5,15 +5,21 @@ CompView::CompView(){}
 
 void CompView::updateCompView(float deltaS){}
 
-void CompView::attackEnemies()
+void CompView::attackEnemies(){
+
+}
 
 // if there is a collsion subtracting health vs damage
 // if unit makes it to exit (grid space 0) subtract remain health from player health and then set player health.
 
-void CompView::moveUnits()
+void CompView::moveUnits(){
+
+}
 
 // Poll the actor class for its speed and position on map, use the shortest distance >= 0
 //	and move that direction
 
-void CompView::delegateEvents()
+void CompView::delegateEvents(){
+
+}
 
