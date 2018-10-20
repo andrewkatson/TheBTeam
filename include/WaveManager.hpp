1aff2d5a3c643923302b98b6e7e3387730b29901
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-10-20 12:56:25 -0400

    added WaveManager skeleton

--- include/WaveManager.hpp
+++ include/WaveManager.hpp
@@ -8,14 +8,30 @@
 #ifndef WAVEMANAGER_H
 #define WAVEMANAGER_H
 
+using namespace std;
+
+#include <memory>
+#include <queue>
+#include "ActorInterface.hpp"
+
 class WaveManager{
 
 private:
-  
+  queue<shared_ptr<ActorInterface>> enemies;
+  queue<queue<shared_ptr<ActorInterface>>> waves;
   
 public:
-  
+  queue<shared_ptr<ActorInterface>> getNextWave(){return waves.front();}
 
+  queue<queue<shared_ptr<ActorInterface>>> getWaves(){return waves;}
+
+  void startNextWave();
+
+  void spawnNextUnit();
+
+  void endCurrentWave();
+
+  void update(float deltaS);
 };
 
 #endif
--- src/WaveManager.cpp
+++ src/WaveManager.cpp
@@ -6,3 +6,22 @@
   @author Jeremy Elkayam
  */
 #include "WaveManager.hpp"
+
+void WaveManager::startNextWave() {
+    queue<shared_ptr<ActorInterface>> next_wave = waves.front();
+    //TODO - code for spawning waves
+}
+
+void WaveManager::endCurrentWave() {
+    //TODO - code to stop the wave
+    waves.pop();
+}
+
+void WaveManager::spawnNextUnit() {
+    shared_ptr<ActorInterface> next_unit = enemies.front();
+    //TODO - spawn the unit
+}
+
+void WaveManager::update(float deltaS) {
+    //TODO - implement
+}
