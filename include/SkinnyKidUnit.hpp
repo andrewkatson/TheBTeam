dcb398da13d27c93fca25c15bb256ee541b06c5c
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-10-26 12:20:01 -0400

    add thin child

---
+++ include/SkinnyKidUnit.hpp
@@ -0,0 +1,16 @@
+//
+// Created by jeremy on 10/26/18.
+//
+
+#ifndef CSCI437_SKINNYKIDUNIT_HPP
+#define CSCI437_SKINNYKIDUNIT_HPP
+
+#include "MeleeUnit.hpp"
+
+class SkinnyKidUnit : public MeleeUnit {
+public:
+  SkinnyKidUnit();
+};
+
+
+#endif //CSCI437_SKINNYKIDUNIT_HPP
