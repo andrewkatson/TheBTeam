00a0e297c82ecc851484fdc930c3c2c0aa092cf1
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-10-26 12:01:18 -0400

    added fat kid unit

---
+++ include/FatKidUnit.hpp
@@ -0,0 +1,21 @@
+//
+// Created by jeremy on 10/26/18.
+//
+
+#ifndef CSCI437_FATKIDUNIT_HPP
+#define CSCI437_FATKIDUNIT_HPP
+
+
+class FatKidUnit : public MeleeUnit {
+
+private:
+
+public:
+
+  FatKidUnit();
+
+
+};
+
+
+#endif //CSCI437_FATKIDUNIT_HPP
---
+++ src/FatKidUnit.cpp
@@ -0,0 +1,11 @@
+//
+// Created by jeremy on 10/26/18.
+//
+
+#include "FatKidUnit.hpp"
+
+FatKidUnit::FatKidUnit(){
+  hitpoints=300;
+  damage=40;
+  armor=100;
+}
