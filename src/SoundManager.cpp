532f6b1af05dd9643d138d1486baa7c81d9cb60f
Jeremy Elkayam <jeremyelkayam@gmail.com>
2018-12-04 00:19:34 -0500

    adjust volume

--- src/SoundManager.cpp
+++ src/SoundManager.cpp
@@ -108,12 +108,14 @@ void SoundManager::loadSounds(){
     sf::Music *music=new sf::Music();
     assert(music->openFromFile(textLoader->getString(string("IDS_Combat_")+std::to_string(z)+string("_Music_Path"))));
     music->setLoop(true);
+    music->setVolume(80);
     music_objs[COMBAT].push_back(music);
   }
   for(int z=0;z<=1;z++){
     sf::Music *music=new sf::Music();
     assert(music->openFromFile(textLoader->getString(string("IDS_Prep_")+std::to_string(z)+string("_Music_Path"))));
     music->setLoop(true);
+    music->setVolume(80);
     music_objs[PREP].push_back(music);
   }
 
